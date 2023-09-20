7ae3baaee6612523f3a38725f893be7e15d82867
abdoomr2003 <abdoomer1112003@gmail.com>
2023-09-21 00:35:37 +0300

    Abdelrahman failes

---
+++ aifunctions.c
@@ -0,0 +1,158 @@
+#include "aishell.h"
+/**
+ * display_prompt - function to display "AIprompt$ ".
+*/
+void display_prompt(void)
+{
+	write(STDOUT_FILENO, PROMPT, strlen(PROMPT));
+}
+/**
+ * read_command - read input from enteractive
+ * Return: line ia a command what user write
+*/
+char *read_command(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+
+	ssize_t read = getline(&line, &len, stdin);
+
+	if (read == -1)
+	{
+		free(line);
+		return (NULL);
+	}
+
+	if (line[read - 1] == '\n')
+	{
+		line[read - 1] = '\0';
+	}
+	return (line);
+}
+/**
+ * non_inter_read_command - read from non-enteractive
+ * Return: line ia a command what input
+*/
+char *non_inter_read_command(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+
+	ssize_t read = getline(&line, &len, stdin);
+
+	if (read == -1)
+	{
+		free(line);
+		return (NULL);
+	}
+
+	if (line[read - 1] == '\n')
+	{
+		line[read - 1] = '\0';
+	}
+	return (line);
+}
+/**
+ * execute_command - check and Execute a command.
+ * @command: A ptr to a char str represent the command to execute.
+ * @num_loop: An int represent the loop number.
+ * @argv: A pointer to a character string representing the name of the program.
+ *
+ * Return: The exit status of the executed command.
+ */
+int execute_command(char *command, int num_loop, char *argv)
+{
+	pid_t child_pid;
+	int child_exit = 0, status;
+	char *full_path;
+	char *exec_args[2];
+
+	full_path = find_path(command);
+	if (full_path == NULL)
+	{
+		handle_exist_error(num_loop, command, argv);
+		return (127);
+	} child_pid = fork();
+	if (child_pid == -1)
+	{
+		perror("fork");
+		free(full_path);
+		_exit(-1);
+	} else if (child_pid == 0)
+	{
+		exec_args[0] = full_path;
+		exec_args[1] = NULL;
+		if (execve(full_path, exec_args, NULL) == -1)
+		{
+			perror("execve");
+			free(full_path);
+			_exit(127);
+		}
+	} else if (child_pid > 0)
+	{
+		if (waitpid(child_pid, &status, 0) == -1)
+		{
+			perror("waitpid");
+			free(full_path);
+			_exit(-1);
+		} else if (WIFEXITED(status))
+			child_exit = WEXITSTATUS(status);
+	}
+	free(full_path);
+	return (child_exit);
+}
+/**
+ *find_path - find path for command
+ *@command: command that given by usr
+ *
+ *Return: A pointer to the full path of the command if found
+ *		or NULL if not found.
+*/
+char *find_path(char *command)
+{
+	char *path_value = get_environment_variable("PATH");
+	char *path_copy, *path_token, *full_path = NULL;
+
+	if (command == NULL || path_value == NULL)
+		return (NULL);
+	path_copy = _strdup(path_value);
+	if (path_copy == NULL)
+		return (NULL);
+	if (command[0] == '/' || command[0] == '.')
+	{
+		if (access(command, F_OK) == 0)
+			return (_strdup(command));
+		return (NULL);
+	}
+	path_token = strtok(path_copy, ":");
+	if (path_token == NULL)
+	{
+		free(path_copy);
+		return (NULL);
+	}
+	while (path_token != NULL)
+	{
+		size_t file_path_len = _strlen(path_token) + _strlen(command) + 2;
+
+		full_path = (char *)malloc(file_path_len);
+		if (full_path == NULL)
+		{
+			free(path_copy);
+			return (NULL);
+		} _strcpy(full_path, path_token);
+		_strcat(full_path, "/");
+		_strcat(full_path, command);
+		if (access(full_path, F_OK) == 0)
+		{
+			free(path_copy);
+			return (full_path);
+		} free(full_path);
+		path_token = strtok(NULL, ":");
+	} free(path_copy);
+	return (NULL);
+}
+
+
+
+
+
---
+++ path_functions.c
@@ -0,0 +1,66 @@
+#include "aishell.h"
+/**
+ * check_current_directory - Check if a command exists.
+ * @command: A pointer to a character string representing the command.
+ *
+ * Return: If the file/command exists in the current directory,
+ *			return a pointerto the same command. Otherwise, return NULL.
+*/
+char *check_current_directory(char *command)
+{
+	int exist;
+
+	if (command == NULL)
+		return (NULL);
+	exist = access(command, F_OK);
+	if (exist == 0)
+		return (command);
+	return (NULL);
+}
+/**
+ * search_in_path - Search for a command in the directories listed in PATH.
+ * @command: A pointer to a char str represent the command that search for.
+ * @path: A pointer to a char str contain the PATH variable.
+ *
+ * Return: If the command is found in any of the directories in PATH, return
+ *         a pointer to the full path of the command. else, return NULL.
+ */
+char *search_in_path(char *command, char *path)
+{
+	char *path_copy, *path_token, *file_path;
+	size_t file_path_len;
+
+	if (command == NULL || path == NULL)
+		return (NULL);
+	path_copy = _strdup(path);
+	if (path_copy == NULL)
+		return (NULL);
+	path_token = strtok(path_copy, ":");
+	while (path_token != NULL)
+	{
+		file_path_len = _strlen(path_token) + _strlen(command) + 2;
+		file_path = (char *)malloc(file_path_len);
+
+		if (file_path == NULL)
+		{
+			free(path_copy);
+			return (NULL);
+		}
+
+		_strcpy(file_path, path_token);
+		_strcat(file_path, "/");
+		_strcat(file_path, command);
+
+		if (access(file_path, F_OK) == 0)
+		{
+			free(path_copy);
+			return (file_path);
+		}
+
+		path_token = strtok(NULL, ":");
+		free(file_path);
+	}
+	free(path_copy);
+	return (NULL);
+}
+
